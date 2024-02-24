be6427c0842b52fe4491ccab612b4ce5ed115b6c
Brandon Ferrer <brandonferrer@brandons-mbp-14.lan>
2024-02-23 23:40:14 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Brandon Ferrer Web`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
